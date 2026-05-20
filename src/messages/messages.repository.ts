4281e9ea3c76c75bf637c3c1c39670bc69e445e3
Ákos Osvald <elettartam@gmail.com>
2026-05-20 20:07:35 +0200

    feat: service and repo are added

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class MessagesRepository {
+    async findOne(id: string) {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+        return messages[id];
+    }
+
+    async findAll() {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+        return messages;
+    }
+
+    async create(content: string) {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+        const id = Math.random().toString(36).substring(2, 15);
+        messages[id] = { id, content };
+        await writeFile('messages.json', JSON.stringify(messages));
+    }
+}
